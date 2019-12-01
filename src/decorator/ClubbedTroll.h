cfe8fa7e06a2ab2001be69e076e6aea805e65d99
Samuel Nuttall <sndrummer@gmail.com>
2019-11-30 18:16:26 -0700

    Added comments to explain decoration

--- src/decorator/ClubbedTroll.h
+++ src/decorator/ClubbedTroll.h
@@ -8,9 +8,16 @@
 class ClubbedTroll : public ITroll
 {
 public:
+	// Uses composition to wrap a Troll class
 	explicit ClubbedTroll(ITroll* troll);
+
+	// Delegates the getName to the ITroll member
 	const std::string& getName() const override;
+
+	// Adds functionality to the attack
 	void attack() override;
+
+	// Default destructor
 	~ClubbedTroll() override = default;
 
 private:
